57cce042d7fc66919547277a8d5744c637d2c5fd
Manuel De Alba <ferman5903@gmail.com>
2023-10-16 16:33:47 -0600

    Cerrar sesión, navegacion diferente si tiene sesión activa

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -18,72 +18,7 @@
             <div id="fondoModal" class="fixed inset-0 z-20 transition-opacity bg-black opacity-50 lg:hidden"></div>
             
             {{-- Sidebar --}}
-            <div id="sidebar" class="fixed inset-y-0 left-0 z-30 w-64 overflow-y-auto transition duration-300 transform bg-gray-900 lg:translate-x-0 lg:static lg:inset-0">
-                <div class="flex items-center justify-center mt-8 w-[90%] mx-auto">
-                    <div class="flex items-center">
-                        <img src="{{ asset('assets/logo.png') }}" class="w-12 h-12" alt="Logo Home Stock Essentials">
-                        
-                        <span class="mx-2 text-xl font-semibold text-white text-center">Home stock essentials</span>
-                    </div>
-                </div>
-        
-                <nav class="mt-10">
-                    <a href="/" class="flex items-center px-6 py-2 mt-4 text-gray-500 hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100">
-                        <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                            stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10">
-                            </path>
-                        </svg>
-        
-                        <span class="mx-3">Inicio</span>
-                    </a>
-        
-                    <a href="{{ route('productos.index') }}" class="flex items-center px-6 py-2 mt-4 text-gray-500 hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100">
-                        <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                            stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="M17 14v6m-3-3h6M6 10h2a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v2a2 2 0 002 2zm10 0h2a2 2 0 002-2V6a2 2 0 00-2-2h-2a2 2 0 00-2 2v2a2 2 0 002 2zM6 20h2a2 2 0 002-2v-2a2 2 0 00-2-2H6a2 2 0 00-2 2v2a2 2 0 002 2z">
-                            </path>
-                        </svg>
-        
-                        <span class="mx-3">Productos</span>
-                    </a>
-        
-                    <a href="{{ route('productos.create') }}" class="flex items-center px-6 py-2 mt-4 text-gray-500 hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100">
-                        <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                            stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z">
-                            </path>
-                        </svg>
-        
-                        <span class="mx-3">Crear producto</span>
-                    </a>
-
-                    <a href="{{ route('categorias.index') }}" class="flex items-center px-6 py-2 mt-4 text-gray-500 hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100">
-                        <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                            stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="M17 14v6m-3-3h6M6 10h2a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v2a2 2 0 002 2zm10 0h2a2 2 0 002-2V6a2 2 0 00-2-2h-2a2 2 0 00-2 2v2a2 2 0 002 2zM6 20h2a2 2 0 002-2v-2a2 2 0 00-2-2H6a2 2 0 00-2 2v2a2 2 0 002 2z">
-                            </path>
-                        </svg>
-        
-                        <span class="mx-3">Categorías</span>
-                    </a>
-
-                    <a href="{{ route('categorias.create') }}" class="flex items-center px-6 py-2 mt-4 text-gray-500 hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100">
-                        <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                            stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z">
-                            </path>
-                        </svg>
-        
-                        <span class="mx-3">Crear categoría</span>
-                    </a>
-                </nav>
-            </div>
+            <x-sidebar />
             
             <div class="flex flex-col flex-1 overflow-hidden">
                 {{-- Barra de navegación --}}
@@ -114,8 +49,27 @@
 
                     {{-- Inicio de sesión --}}
                     <div class="space-x-2">
-                        <a href="/register">Registrarse</a>
-                        <a href="/login">Iniciar sesión</a>
+                        {{-- Se usa el helper auth para obtener el usuario actual --}}
+                        {{-- Si no existe muestra el inicio de sesión --}}
+                        {{-- Si existe muestra cerrar sesión y el nombre --}}
+                        {{-- @if (!auth()->user()) --}}
+                        {{-- @endif --}}
+
+                        {{-- También se puede usar la directiva @auth --}}
+                        @auth
+                            <span>{{ auth()->user()->name }}</span>
+                            <form id="form-logout" class="inline" action="{{ url('/logout') }}" method="POST">
+                                @csrf
+                                <a href="{{ url('/logout') }}" onclick="cerrarSesion(event)">Cerrar sesión</a>
+                            </form>
+                        @else
+                            <a href="/register">Registrarse</a>
+                            <a href="/login">Iniciar sesión</a>
+                        @endauth
+
+                        {{-- Guest es el opuesto de auth --}}
+                        {{-- @guest
+                        @endguest --}}
                     </div>
                 </header>
 
@@ -145,6 +99,11 @@
             sidebar.classList.remove("ease-in");
             fondoModal.classList.remove("hidden");
         })
+
+        function cerrarSesion(e){
+            e.preventDefault();
+            document.getElementById('form-logout').submit();
+        }
     </script>
 </body>
 </html>
